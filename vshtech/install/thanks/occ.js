9ff3350fcbc27fee11eb32d2ef03a3ec502d5452
vshtechteam <vshtechteam@gmail.com>
2025-11-08 22:59:41 +0700

    Add files via upload

---
+++ vshtech/install/thanks/occ.js
@@ -0,0 +1,10 @@
+// Đổi đường dẫn này nếu tên file khác (ví dụ '/profiles/8.mobileconfig')
+const FILE = '/profiles/vpn.mobileconfig';
+
+const retryBtn = document.getElementById('retry');
+if (retryBtn) {
+  retryBtn.addEventListener('click', () => {
+    window.location.assign(FILE);
+    setTimeout(() => { location.reload(); }, 600);
+  });
+}
