5ef54df8d3fa5ede298cb4a98d2277099e7adadc
vshtechteam <vshtechteam@gmail.com>
2025-12-19 23:43:58 +0700

    Delete vshtech/panel directory

--- vshtech/panel/r.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  const b64 = "aHR0cHM6Ly93d3cuYXBwc3RhY2suYmxvZy9hcHAvbWVudS1hcHAtaW9zLWRldmVsb3BtZW50LWFwcGxlLWJsYWNrbGlzdGVkLw==";
-  const url = atob(b64);
-  location.replace(url);
-})();
